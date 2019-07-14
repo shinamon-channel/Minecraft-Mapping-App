9b3478e72139c190caca217a4105b3b5b0f40e1e
K4rdamon <k4rd4m0n@gmail.com>
2019-07-15 01:00:09 +0900

    test: add 'www/tags/test/test_data.js'

---
+++ www/tags/test/test_data.js
@@ -0,0 +1,35 @@
+module.exports = {
+  hotbar_test: [
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: false
+    },
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: false
+    },
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: true
+    },
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: false
+    },
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: false
+    },
+    {
+      img_path: "hoge/fuga.png",
+      is_writing: false
+    }
+  ],
+  maps_test: [{
+    id: 'hogehoge'
+  },{
+    id: 'fugafuga'
+  },{
+    id: 'piyopiyo'
+  }]
+}
